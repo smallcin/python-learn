cce22bf658e75009fb4633289ca00009cd6d783b
张文军 <759266178@qq.com>
2018-03-21 08:56:31 +0800

    Mac run python

---
+++ 第一章安装和运行python/Mac/Mac_run_python.py
@@ -0,0 +1,17 @@
+#打开iTerm2 命令行，个人比较习惯这个工具，虽然苹果自带的也比较好用吧。。。
+#输入python
+# Python 2.7.10 (default, Jul 15 2017, 17:16:57)
+# [GCC 4.2.1 Compatible Apple LLVM 9.0.0 (clang-900.0.31)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>>
+
+#现在就是进入python shell中了，python已经运行等待输入命令。
+
+#输入print "Hello World!" 就可以打印第一条程序语句了。
+# >>> print "Hello World!"
+# Hello World!
+# >>>
+
+#输入exit()就可以退出python shell
+
+
